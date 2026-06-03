df6c3c0dd53c2383a561781c2996dbe3743f7e82
Frederic Pillon <frederic.pillon@st.com>
2026-06-03 15:30:08 +0200

    chore: update to stm32_mw_fatfs v4.0.4
    FatFs R0.15 modified by ST
    Tag: r0.15_stm32cube_20260220
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- src/drivers/sd_diskio.c
+++ src/drivers/sd_diskio.c
@@ -6,19 +6,17 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2017-2019 STMicroelectronics. All rights reserved.
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                       opensource.org/licenses/BSD-3-Clause
+  * This software is licensed under terms that can be found in the st_license.txt
+  * file in the root directory of this software component.
+  * If no st_license.txt file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
-**/
+  */
 /* Includes ------------------------------------------------------------------*/
-#include "ff_gen_drv.h"
 #include "sd_diskio.h"
-
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -46,33 +44,29 @@
 static volatile DSTATUS Stat = STA_NOINIT;
 
 /* Private function prototypes -----------------------------------------------*/
-static DSTATUS SD_CheckStatus(BYTE lun);
-DSTATUS SD_initialize (BYTE);
-DSTATUS SD_status (BYTE);
-DRESULT SD_read (BYTE, BYTE*, DWORD, UINT);
-#if _USE_WRITE == 1
-  DRESULT SD_write (BYTE, const BYTE*, DWORD, UINT);
-#endif /* _USE_WRITE == 1 */
-#if _USE_IOCTL == 1
-  DRESULT SD_ioctl (BYTE, BYTE, void*);
-#endif  /* _USE_IOCTL == 1 */
+static DSTATUS SD_check_status (BYTE);
+static DSTATUS SD_initialize (BYTE);
+static DSTATUS SD_status (BYTE);
+static DRESULT SD_read (BYTE, BYTE*, LBA_t, UINT);
+static DRESULT SD_write (BYTE, const BYTE*, LBA_t, UINT);
+static DRESULT SD_ioctl (BYTE, BYTE, void*);
 
 const Diskio_drvTypeDef  SD_Driver =
 {
   SD_initialize,
   SD_status,
   SD_read,
-#if _USE_WRITE == 1
   SD_write,
-#endif /* _USE_WRITE == 1 */
-
-#if _USE_IOCTL == 1
   SD_ioctl,
-#endif /* _USE_IOCTL == 1 */
 };
 
 /* Private functions ---------------------------------------------------------*/
-static DSTATUS SD_CheckStatus(BYTE lun)
+/**
+ * @brief  Check the status of the sd card
+ * @param  lun : not used
+ * @retval DSTATUS: return 0 if the sd card is ready and 1 otherwise
+ */
+static DSTATUS SD_check_status(BYTE lun)
 {
   (void)lun;
   Stat = STA_NOINIT;
@@ -86,45 +80,43 @@ static DSTATUS SD_CheckStatus(BYTE lun)
 }
 
 /**
-  * @brief  Initializes a Drive
-  * @param  lun : not used
-  * @retval DSTATUS: Operation status
-  */
-DSTATUS SD_initialize(BYTE lun)
+ * @brief  Initialize the SD Diskio lowlevel driver
+ * @param  lun : not used
+ * @retval DSTATUS: return 0 on Success STA_NOINIT otherwise
+ */
+static DSTATUS SD_initialize(BYTE lun)
 {
   Stat = STA_NOINIT;
 #if !defined(DISABLE_SD_INIT)
-
   if(BSP_SD_Init() == MSD_OK)
   {
-    Stat = SD_CheckStatus(lun);
+    Stat = SD_check_status(lun);
   }
-
 #else
-  Stat = SD_CheckStatus(lun);
+  Stat = SD_check_status(lun);
 #endif
   return Stat;
 }
 
 /**
-  * @brief  Gets Disk Status
-  * @param  lun : not used
-  * @retval DSTATUS: Operation status
-  */
-DSTATUS SD_status(BYTE lun)
+ * @brief  Get the Disk Status
+ * @param  lun : not used
+ * @retval DSTATUS: Operation status
+ */
+static DSTATUS SD_status(BYTE lun)
 {
-  return SD_CheckStatus(lun);
+  return SD_check_status(lun);
 }
 
 /**
-  * @brief  Reads Sector(s)
-  * @param  lun : not used
-  * @param  *buff: Data buffer to store read data
-  * @param  sector: Sector address (LBA)
-  * @param  count: Number of sectors to read (1..128)
-  * @retval DRESULT: Operation result
-  */
-DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
+ * @brief  Reads Sector(s)
+ * @param  lun : not used
+ * @param  *buff: Data buffer to store read data
+ * @param  sector: Sector address (LBA)
+ * @param  count: Number of sectors to read (1..128)
+ * @retval DRESULT: return RES_OK otherwise
+ */
+static DRESULT SD_read(BYTE lun, BYTE *buff, LBA_t sector, UINT count)
 {
   (void)lun;
   DRESULT res = RES_ERROR;
@@ -133,26 +125,24 @@ DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
                        (uint32_t) (sector),
                        count, SD_TIMEOUT) == MSD_OK)
   {
-    /* wait until the read operation is finished */
+    /* Wait until the card state is ready */
     while(BSP_SD_GetCardState()!= MSD_OK)
     {
     }
     res = RES_OK;
   }
-
   return res;
 }
 
 /**
-  * @brief  Writes Sector(s)
-  * @param  lun : not used
-  * @param  *buff: Data to be written
-  * @param  sector: Sector address (LBA)
-  * @param  count: Number of sectors to write (1..128)
-  * @retval DRESULT: Operation result
-  */
-#if _USE_WRITE == 1
-DRESULT SD_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
+ * @brief  Write data from sd card into a buffer
+ * @param  lun : not used
+ * @param  *buff: Data to be written
+ * @param  sector: Sector address (LBA)
+ * @param  count: Number of sectors to write (1..128)
+ * @retval DRESULT: return RES_OK otherwise
+ */
+static DRESULT SD_write(BYTE lun, const BYTE *buff, LBA_t sector, UINT count)
 {
   (void)lun;
   DRESULT res = RES_ERROR;
@@ -161,26 +151,23 @@ DRESULT SD_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
                         (uint32_t)(sector),
                         count, SD_TIMEOUT) == MSD_OK)
   {
-	/* wait until the Write operation is finished */
+    /* Wait until the card state is ready */
     while(BSP_SD_GetCardState() != MSD_OK)
     {
     }
     res = RES_OK;
   }
-
   return res;
 }
-#endif /* _USE_WRITE == 1 */
 
 /**
-  * @brief  I/O control operation
-  * @param  lun : not used
-  * @param  cmd: Control code
-  * @param  *buff: Buffer to send/receive control data
-  * @retval DRESULT: Operation result
-  */
-#if _USE_IOCTL == 1
-DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff)
+ * @brief  I/O control operation
+ * @param  lun : not used
+ * @param  cmd: Control code
+ * @param  *buff: Buffer to send/receive control data
+ * @retval DRESULT: return RES_OK otherwise
+ */
+static DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff)
 {
   (void)lun;
   DRESULT res = RES_ERROR;
@@ -191,7 +178,7 @@ DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff)
   switch (cmd)
   {
   /* Make sure that no pending write process */
-  case CTRL_SYNC :
+  case CTRL_SYNC:
     res = RES_OK;
     break;
 
@@ -222,7 +209,3 @@ DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff)
 
   return res;
 }
-#endif /* _USE_IOCTL == 1 */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
--- src/drivers/sd_diskio.h
+++ src/drivers/sd_diskio.h
@@ -2,30 +2,37 @@
   ******************************************************************************
   * @file    sd_diskio.h
   * @author  MCD Application Team
-  * @brief   Header for sd_diskio.c module.
+  * @brief   Header for sd_diskio.c module
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2017 STMicroelectronics. All rights reserved.
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                       opensource.org/licenses/BSD-3-Clause
+  * This software is licensed under terms that can be found in the st_license.txt
+  * file in the root directory of this software component.
+  * If no st_license.txt file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
-**/
+  */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __SD_DISKIO_H
 #define __SD_DISKIO_H
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
 /* Includes ------------------------------------------------------------------*/
+#include "ff_gen_drv.h"
+
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-extern const Diskio_drvTypeDef  SD_Driver;
+extern const Diskio_drvTypeDef SD_Driver;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __SD_DISKIO_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
